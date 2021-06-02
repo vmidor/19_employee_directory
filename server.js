4d5ad0d1ab0d9ca5be32dfe79f6b3fe49f5760f7
vmidor <76964172+vmidor@users.noreply.github.com>
2021-06-02 16:32:32 -0400

    trying to deployed

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express()
-const PORT = process.env.PORT || 3001;
-
-app.use(express.static('react19/build'))
-
-app.listen(PORT, () => console.log('listening on PORT', PORT))
